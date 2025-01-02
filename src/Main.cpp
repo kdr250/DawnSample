57220c7b0e7b9e6ad3fe8178dc531e21a2bc0c47
kdr250 <54400975+kdr250@users.noreply.github.com>
2025-01-02 12:59:04 +0900

    The Adapter

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <webgpu/webgpu.h>
+#include <cassert>
 
 #ifdef __EMSCRIPTEN__
     #include <emscripten.h>
@@ -10,10 +11,112 @@
 #include "sdl2webgpu.h"
 
 SDL_Window* window;
+WGPUInstance instance;
+WGPUAdapter adapter;
+WGPUSurface surface;
 bool isRunning = true;
+
+WGPUAdapter RequestAdapterSync(WGPUInstance instance, WGPURequestAdapterOptions const* options)
+{
+    struct UserData
+    {
+        WGPUAdapter adapter = nullptr;
+        bool requestEnded   = false;
+    };
+    UserData userData;
+
+    auto onAdapterRequestEnded = [](WGPURequestAdapterStatus status,
+                                    WGPUAdapter adapter,
+                                    WGPUStringView message,
+                                    void* pUserData)
+    {
+        UserData& userData = *reinterpret_cast<UserData*>(pUserData);
+        if (status == WGPURequestAdapterStatus_Success)
+        {
+            userData.adapter = adapter;
+        }
+        else
+        {
+            SDL_Log("Could not get WebGPU adapter: %s", message.data);
+        }
+        userData.requestEnded = true;
+    };
+
+    wgpuInstanceRequestAdapter(instance, options, onAdapterRequestEnded, (void*)&userData);
+
+#ifdef __EMSCRIPTEN__
+    while (!userData.requestEnded)
+    {
+        emscripten_sleep(100);
+    }
+#endif
+
+    assert(userData.requestEnded);
+
+    return userData.adapter;
+}
+
+void InspectAdapter(WGPUAdapter adapter)
+{
+#ifndef __EMSCRIPTEN__
+    WGPUSupportedLimits supportedLimits = {};
+    supportedLimits.nextInChain         = nullptr;
+
+    WGPUStatus status = wgpuAdapterGetLimits(adapter, &supportedLimits);
+    if (status == WGPUStatus_Success)
+    {
+        SDL_Log("Adapter limits:");
+        SDL_Log(" - maxTextureDimension1D: %d", supportedLimits.limits.maxTextureDimension1D);
+        SDL_Log(" - maxTextureDimension2D: %d", supportedLimits.limits.maxTextureDimension2D);
+        SDL_Log(" - maxTextureDimension3D: %d", supportedLimits.limits.maxTextureDimension3D);
+        SDL_Log(" - maxTextureArrayLayers: %d", supportedLimits.limits.maxTextureArrayLayers);
+    }
+#endif
+
+    WGPUSupportedFeatures features;
+    wgpuAdapterGetFeatures(adapter, &features);
+
+    SDL_Log("Adapter features:");
+    for (int i = 0; i < features.featureCount; ++i)
+    {
+        auto feature = features.features[i];
+        SDL_Log(" - 0x%08X", feature);
+    }
+
+    WGPUAdapterInfo info;
+    info.nextInChain = nullptr;
+    wgpuAdapterGetInfo(adapter, &info);
+
+    SDL_Log("Adapter properties:");
+    SDL_Log(" - vendorID: %i", info.vendorID);
+    if (info.vendor.length > 0)
+    {
+        SDL_Log(" - vendor: %s", info.vendor.data);
+    }
+    if (info.architecture.length > 0)
+    {
+        SDL_Log(" - architecture: %s", info.architecture.data);
+    }
+    SDL_Log(" - deviceID: %i", info.deviceID);
+    if (info.device.length > 0)
+    {
+        SDL_Log(" - device: %s", info.device.data);
+    }
+    if (info.description.length > 0)
+    {
+        SDL_Log(" - description: %s", info.description.data);
+    }
+    SDL_Log(" - adapterType: 0x%08X", info.adapterType);
+    SDL_Log(" - backendType: 0x%08X", info.backendType);
+}
 
 void Quit()
 {
+    // Terminate WebGPU
+    wgpuSurfaceRelease(surface);
+    wgpuAdapterRelease(adapter);
+    wgpuInstanceRelease(instance);
+
     // Terminate SDL
     SDL_DestroyWindow(window);
     SDL_Quit();
@@ -54,7 +157,7 @@ void MainLoop()
 int main(int argc, char* argv[])
 {
     // Init WebGPU
-    WGPUInstance instance = wgpuCreateInstance(nullptr);
+    instance = wgpuCreateInstance(nullptr);
     if (instance == nullptr)
     {
         SDL_Log("Instance creation failed!");
@@ -70,8 +173,15 @@ int main(int argc, char* argv[])
                               768,
                               0);
 
+    // Requesting Adapter
+    WGPURequestAdapterOptions adapterOptions = {};
+    adapterOptions.nextInChain               = nullptr;
+    adapter                                  = RequestAdapterSync(instance, &adapterOptions);
+
+    InspectAdapter(adapter);
+
     // Create WebGPU surface
-    WGPUSurface surface = SDL_GetWGPUSurface(instance, window);
+    surface = SDL_GetWGPUSurface(instance, window);
 
 #ifdef __EMSCRIPTEN__
     emscripten_set_main_loop(MainLoop, 0, 1);
