0b8f686a574aeb68e6dc18501989e78dd62836a2
kdr250 <54400975+kdr250@users.noreply.github.com>
2026-02-12 12:11:04 +0900

    delete unused include

--- src/WebGPUUtils.h
+++ src/WebGPUUtils.h
@@ -3,11 +3,6 @@
 #include <webgpu/webgpu_cpp.h>
 #include <string>
 
-#ifdef __EMSCRIPTEN__
-    #include <emscripten.h>
-    #include <emscripten/html5.h>
-#endif
-
 namespace WebGPUUtils
 {
     /**
